01a5aa513493d5acb4e65facef48fbf016d184b9
Lucas Rosario <lrosari2@illinois.edu>
2016-04-07 18:26:37 -0500

    Fixing goofiness

--- GenomeAssembly/src/GenomeSolver.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by lucasrosario on 4/7/16.
- */
-public class GenomeSolver {
-
-    public static String solve(String[] str){
-        return null;
-    }
-}
--- GenomeAssembly/src/MainTest.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by lucasrosario on 4/7/16.
- */
-public class MainTest {
-
-    public static void main(String[] args){
-        System.out.println("Almost Done");
-    }
-}
